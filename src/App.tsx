1f43c86571a211e4d2dc493cb18e3466547c79a0
JeonB <newjeonb@gmail.com>
2023-12-11 22:15:24 +0900

    mod uncommon

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,124 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable */
+import React, { useState, useEffect } from 'react';
+import Uncommon from './composition/Uncommon';
+interface KeyboardCounterProps {
+  children: React.ReactNode;
+}
 
-function App() {
+interface CharacterKeys {
+  q: string;
+  w: string;
+  e: string;
+  r: string;
+  a: string;
+  s: string;
+  d: string;
+  f: string;
+  g: string;
+  [key: string]: string; // 인덱스 시그니처 추가
+}
+
+const KeyboardCounter: React.FC = () => {
+  const characterKeys: CharacterKeys = {
+    q: '루피',
+    w: '조로',
+    e: '나미',
+    r: '우솝',
+    a: '상디',
+    s: '쵸파',
+    d: '버기',
+    f: '총병',
+    g: '칼병',
+  };
+
+  // 흔함 유닛 수
+  const initialCount = {
+    루피: 0,
+    조로: 0,
+    나미: 0,
+    우솝: 0,
+    상디: 0,
+    쵸파: 0,
+    버기: 0,
+    총병: 0,
+    칼병: 0,
+  };
+
+  const [count, setCount] = useState(initialCount);
+
+  // 흔함 유닛에 해당하는 키를 누를 시 기존 개수에 1을 더함
+  const handleKeyPress = (event: KeyboardEvent) => {
+    const key = event.key.toLowerCase();
+    if (characterKeys[key]) {
+      setCount((prevCount: Record<string, number>) => {
+        const updatedCount: { [key: string]: number } = { ...prevCount };
+        updatedCount[characterKeys[key]] =
+          (prevCount[characterKeys[key]] || 0) + 1;
+        return updatedCount as {
+          루피: number;
+          조로: number;
+          나미: number;
+          우솝: number;
+          상디: number;
+          쵸파: number;
+          버기: number;
+          총병: number;
+          칼병: number;
+        }; // 타입 단언 추가
+      });
+    }
+  };
+
+  useEffect(() => {
+    // 컴포넌트가 마운트될 때 이벤트 리스너를 추가
+    window.addEventListener('keydown', handleKeyPress);
+    return () => {
+      // 컴포넌트가 언마운트될 때 이벤트 리스너를 제거
+      window.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [count, initialCount]); // Include 'initialCount' and 'completion' in the dependency array
+
+  const handleReset = () => {
+    setCount(initialCount);
+  };
+
+  const handleCharacterReset = (character: string) => {
+    setCount((prevCount) => ({
+      ...prevCount,
+      [character]: 0,
+    }));
+  };
+  return (
+    <div>
+      <p>원랜디 조합식 - 키를 눌러 흔함의 개수 증가</p>
+      {Object.entries(characterKeys).map(([key, character]) => (
+        <div key={key}>
+          <p>
+            {`${character} ${key}: ${count[character as keyof typeof count]}`}
+
+            <button
+              style={{ marginLeft: 20 }}
+              onClick={() => handleCharacterReset(character)}
+            >
+              초기화
+            </button>
+          </p>
+        </div>
+      ))}
+      <Uncommon initialCount={count} />
+      <button onClick={handleReset}>모두 초기화</button>
+    </div>
+  );
+};
+
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <KeyboardCounter />
       </header>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/composition/Uncommon.tsx
@@ -0,0 +1,70 @@
+/* eslint-disable */
+import React, { useState, useEffect } from 'react';
+
+interface UncommonProps {
+  initialCount: Record<string, number>;
+}
+const Uncommon: React.FC<UncommonProps> = ({ initialCount }) => {
+  const composition = {
+    후쿠로: { 칼병: 2 },
+    블루노: { 총병: 2 },
+    니코로빈: { 나미: 1, 상디: 1 },
+    베포: { 루피: 1, 쵸파: 1 },
+    브룩: { 조로: 1, 쵸파: 1 },
+    이나즈마: { 조로: 1, 상디: 1 },
+    저격왕: { 우솝: 2 },
+    타시기: { 총병: 1, 칼병: 1 },
+    럼블볼: { 쵸파: 2 },
+    페로나: { 나미: 1, 버기: 1 },
+    에이스: { 루피: 1, 총병: 1 },
+    프랑키: { 루피: 1, 우솝: 1 },
+    하찌: { 나미: 1, 총병: 1 },
+  };
+
+  const [completion, setCompletion] = useState<{ [key: string]: number }>(
+    Object.fromEntries(Object.keys(composition).map((unit) => [unit, 0])),
+  );
+
+  useEffect(() => {
+    // 각 유닛의 개수가 증가할 때마다 완성도를 계산
+    const calculateCompletion = () => {
+      const newCompletion = { ...completion };
+
+      Object.keys(composition).forEach((unit) => {
+        const unitConditions = composition[unit];
+        const totalConditions = Object.keys(unitConditions).length;
+        const satisfiedConditions = Object.keys(unitConditions).filter(
+          (condition) =>
+            (initialCount[condition] || 0) >= unitConditions[condition],
+        );
+
+        // Calculate completion percentage only if all conditions are satisfied
+        if (satisfiedConditions.length === totalConditions) {
+          newCompletion[unit] = 100;
+        } else {
+          newCompletion[unit] =
+            (satisfiedConditions.length / totalConditions) * 100;
+        }
+      });
+
+      setCompletion(newCompletion);
+    };
+
+    calculateCompletion();
+  }, [initialCount, completion]); // Include 'initialCount' and 'completion' in the dependency array
+
+  // 키보드 이벤트 핸들러 등을 추가
+
+  return (
+    <div>
+      <p>유닛 완성도</p>
+      {Object.entries(completion).map(([unit, completeness]) => (
+        <div key={unit}>
+          <p>{`${unit}: ${completeness.toFixed(2)}%`}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Uncommon;
