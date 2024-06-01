80e8fb6a6f1441e6683503fac566b8ecf66e635b
JeonB <newjeonb@gmail.com>
2024-06-01 14:41:45 +0900

    webpack 설정 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/index.tsx', // entry point is now a TypeScript file
+  mode: 'development', // Add this line
+  entry: './src/index.tsx',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
@@ -9,15 +10,25 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|ts|tsx)$/, // updated to handle TypeScript and React files
+        test: /\.(js|ts|tsx)$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.css$/, // Add this block to handle CSS files
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
   resolve: {
-    extensions: ['.js', '.ts', '.tsx'], // added TypeScript and React extensions
+    extensions: ['.js', '.ts', '.tsx'],
+    alias: {
+      // Add this block to handle module resolution
+      '@_compositions': path.resolve(__dirname, 'src/compositions'),
+      '@_components/*': path.resolve(__dirname, 'src/components/*'),
+      src: path.resolve(__dirname, 'src'),
+    },
   },
 }
